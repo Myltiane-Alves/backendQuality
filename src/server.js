aefe3fe75d9a0db7344772f1eb4f9c7379d66cf8
Myltiane Alves <myltiane.aux@gmail.com>
2024-12-06 15:49:06 -0300

    refactor: api financeiro  venda digital

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from './app.js';
-// const PORT = process.env.PORT || 6001;
+const PORT = process.env.PORT || 6001;
 // const PORT =  'http://164.152.244.96:6001';
-const PORT =  'https://confidencial-api.vercel.app';
+// const PORT =  'https://confidencial-api.vercel.app';
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta `, PORT);
