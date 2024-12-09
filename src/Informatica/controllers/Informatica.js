428ebff8e58041eb860c2f9198f173b61f26a163
Myltiane Alves <myltiane.aux@gmail.com>
2024-12-09 13:17:07 -0300

    refactor: Informatica

--- src/Informatica/controllers/Informatica.js
+++ src/Informatica/controllers/Informatica.js
@@ -264,7 +264,7 @@ class InformaticaControllers {
             page = page ? page : '';
             pageSize = pageSize ? pageSize : '';
             // http://164.152.245.77:8000/quality/concentrador_homologacao/api/informatica/funcionario-loja.xsjs?pagesize=1000&idEmpresa=&dsNomeFunc=
-            const apiUrl = `${url}/api/informatica/funcionario-loja.xsjs?page=${page}&pagesize=${pageSize}&idEmpresa=${idEmpresa}&dsNomeFunc=${noFuncionarioCPF}`;
+            const apiUrl = `${url}/api/informatica/funcionario-loja.xsjs?byId=${byId}&idEmpresa=${idEmpresa}&dsNomeFunc=${noFuncionarioCPF}&page=${page}&pagesize=${pageSize}`;
             const response = await axios.get(apiUrl)
 
             // const response = await getFuncionariosLoja(byId, idEmpresa, cpf, noFuncionarioCPF, page, pageSize)
