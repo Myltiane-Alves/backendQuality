a931d2800fe98a73a2fb76d413b36284a6607267
Myltiane Alves <myltiane.aux@gmail.com>
2024-12-13 08:51:37 -0300

    refactor: cadastro

--- src/Administrativo/controllers/Administrativo.js
+++ src/Administrativo/controllers/Administrativo.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { dataFormatada } from "../../utils/dataFormatada.js";
 import { getDetalheVendas } from "../Vendas/repositories/detalheVenda.js";
 import { getDetalheVoucherDados } from "../Vouchers/repositories/detalheVoucherDados.js";
-let url = `http://164.152.245.77:8000/quality/concentrador`;
+let url = `http://164.152.245.77:8000/quality/concentrador_homologacao`;
 
 class AdministrativoControllers {
 
