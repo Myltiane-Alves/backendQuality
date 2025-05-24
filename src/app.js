05ff4bbf7f98f3d01034d4a2bf4fd4ce372797f9
Myltiane Alves <myltiane.aux@gmail.com>
2025-05-24 15:33:24 -0300

    refactor: node modules

--- src/app.js
+++ src/app.js
@@ -27,7 +27,6 @@ class App {
                 'Authorization'
             ],
             credentials: true,
-            // preflightContinue: true,
         }));
 
         this.server.use(express.json());
