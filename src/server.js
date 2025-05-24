254ec29bf8f2251d639013c2084a2a2ce3f81441
Myltiane Alves <myltiane.aux@gmail.com>
2025-05-24 16:09:34 -0300

    mudança na vercel

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
-import app from './app.js';
-const PORT = process.env.PORT || 6001;
+// import app from './app.js';
+// const PORT = process.env.PORT || 6001;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta `, PORT);
-});
+// app.listen(PORT, () => {
+//   console.log(`Servidor rodando na porta `, PORT);
+// });
+
+import app from './app.js';
+import serverless from 'serverless-http';
+
+export const handler = serverless(app);
