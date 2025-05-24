5711795ae561efab9e3fedec48c85cb41f041047
Myltiane Alves <myltiane.aux@gmail.com>
2025-05-24 16:00:09 -0300

    feadt: add new

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const PORT = process.env.PORT || 6001;
 // const PORT =  'http://164.152.244.96:6001';
 // const PORT =  'https://confidencial-api.vercel.app';
 
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta `, PORT);
 });
