e25edb3faba577a656e2a89a110e1f03c16e0e71
Myltiane Alves <myltiane.aux@gmail.com>
2025-05-26 19:56:39 -0300

    mudança na vercel

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import app from './app.js';
-const PORT = process.env.PORT || 6001;
+const PORT =  6001;
 // const PORT =  'http://164.152.244.96:6001';
 // const PORT =  'https://confidencial-api.vercel.app';
 
