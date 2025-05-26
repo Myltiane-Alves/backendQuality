709692eb7b99f4eab198ea8fd0eabc6d81660a55
Myltiane Alves <myltiane.aux@gmail.com>
2025-05-26 19:12:19 -0300

    feadt: add new

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
-import app from './app.js';
-const PORT = process.env.PORT || 6001;
+// import app from './app.js';
+// const PORT = process.env.PORT || 6001;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta server.js `, PORT);
-});
+// app.listen(PORT, () => {
+//   console.log(`Servidor rodando na porta server.js `, PORT);
+// });
+
+import app from '../src/app.js';
+
+export default app;
