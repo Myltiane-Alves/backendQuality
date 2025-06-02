337684786e05e3ba751ad171fe63c1db6bfb4362
Myltiane Alves <myltiane.aux@gmail.com>
2025-06-02 11:22:20 -0300

    feat: add getDetalheEmpresaPromocaoAtiva

--- src/Promocao/repositories/detalhePromocaoAtiva.js
+++ src/Promocao/repositories/detalhePromocaoAtiva.js
@@ -35,6 +35,53 @@ export const getDetalheEmpresaPromocaoAtiva = async (idResumoPromocao) =>  {
         return data;
         
     } catch (error) {
+        console.log('Erro ao consultar Empresas Promoções Ativas', error);
+    }
+}
+
+export const getDetalhePromocaoAtiva = async (idResumoPromocao, page, pageSize) =>  {
+    try {
+        page = page && !isNaN(page) ? parseInt(page) : 1;
+        pageSize = pageSize && !isNaN(pageSize) ? parseInt(pageSize) : 1000;
+
+        let query = `
+            SELECT 
+                IDDETALHEPROMO,
+                IDRESUMOPROMO, 
+                IDPRODUTO     
+            FROM "${databaseSchema}".DETALHEPROMOCAO
+            WHERE 
+            1 = ?
+
+        `;
+
+        const params = [idResumoPromocao];
+
+        query += ` ORDER BY  IDRESUMOPROMOCAOMARKETING`;
+    
+        const statement = await conn.prepare(query);
+        const result = await statement.exec(params);
+        if(!Array.isArray(result) || result.length === 0) {
+            return [];
+        }
+
+        const data =  await Promise.all(result.map(async (row) => ({
+            detalhe: {
+                idResumoPromocao: row.IDRESUMOPROMO,
+                idDetalhePromocao: row.IDDETALHEPROMO,
+                idProduto: row.IDPRODUTO
+            },
+            empresaspromocaoAtiva: await getDetalheEmpresaPromocaoAtiva(row.IDRESUMOPROMO)
+
+        })));
+        return {
+            page,
+            pageSize,
+            rows: data.length,
+            data: data
+        }
+        
+    } catch (error) {
         console.log('Erro ao consultar Promoções Ativas', error);
     }
 }
